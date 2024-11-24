c873c3612c1e6d6b4b626db64758919a3c307acd
fullstackjunkie <dirtroadddeveloper@gmail.com>
2024-11-23 19:18:34 -0500

    Working on design

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,32 @@
 async function getWeather(location) {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=EAZ5W4DS635YEAYKRZDTTM359&contentType=json`)
-    response.json().then(function(response) {
-        console.log(response);
-    });
+    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=EAZ5W4DS635YEAYKRZDTTM359&contentType=json`, {mode: 'cors'})
+    return await response.json()
 }
 
-getWeather(32024);
+
+async function displayInfo() {
+    
+    let weather = await getWeather(32024);
+    let container = document.querySelector(".container");
+    let mainP = document.querySelector("#main__P");
+    if(weather.currentConditions.datetime > '18:00' && weather.currentConditions.datetime <= '6:00') {
+        container.classList.remove("daytime");
+        container.classList.add("night");
+    } else {
+        container.classList.remove("night");
+        container.classList.add("daytime");
+    }
+
+
+    // mainImg.src = weather.currentConditions.icon;
+    mainP.innerText = "Condition: " + weather.currentConditions.conditions
+    
+    console.log(weather);
+}
+
+
+displayInfo();
+
+
+
+
